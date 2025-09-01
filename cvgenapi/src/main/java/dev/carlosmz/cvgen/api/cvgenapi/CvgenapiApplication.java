2211e5b0627c21e2cfd97acd9c7f061a67ba70d1
carlosmz96 <carlosmz96esp@gmail.com>
2025-09-01 19:44:03 +0200

    Initial commit: Angular + Spring Boot base projects

---
+++ cvgenapi/src/main/java/dev/carlosmz/cvgen/api/cvgenapi/CvgenapiApplication.java
@@ -0,0 +1,13 @@
+package dev.carlosmz.cvgen.api.cvgenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvgenapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvgenapiApplication.class, args);
+	}
+
+}
---
+++ cvgenapi/src/test/java/dev/carlosmz/cvgen/api/cvgenapi/CvgenapiApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.carlosmz.cvgen.api.cvgenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgenapiApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // The test will pass if the application context loads successfully.
+    }
+}
