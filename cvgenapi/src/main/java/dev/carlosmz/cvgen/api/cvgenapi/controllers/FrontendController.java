8f79ea7d7be47c6ed8b0b93b029dfef5b3d69293
carlosmz96 <carlosmz96esp@gmail.com>
2025-09-28 22:36:01 +0200

    changes on frontendcontroller

--- cvgenapi/src/main/java/dev/carlosmz/cvgen/api/cvgenapi/controllers/FrontendController.java
+++ cvgenapi/src/main/java/dev/carlosmz/cvgen/api/cvgenapi/controllers/FrontendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FrontendController {
 
-    @RequestMapping(value = "/{path:^(?!api$).*$}")
+    @RequestMapping(value = {"/{path:^(?!api|v3|swagger).*}", "/**/{path:^(?!api|v3|swagger).*}"})
     public String redirect() {
         return "forward:/index.html";
     }
