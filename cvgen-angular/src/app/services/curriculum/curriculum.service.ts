32d2afb679f2e66d173142be32aaa3a6dd054289
carlosmz96 <carlosmz96esp@gmail.com>
2025-09-29 11:47:41 +0200

    curriculum service modified

--- cvgen-angular/src/app/services/curriculum/curriculum.service.ts
+++ cvgen-angular/src/app/services/curriculum/curriculum.service.ts
@@ -12,52 +12,35 @@ export class CurriculumService {
 
   cvUrl: string = environment.apiBaseUrl + '/curriculums';
   token: string = '';
+  headers: HttpHeaders;
 
   constructor(
     private httpClient: HttpClient,
     private cookieService: CookieService
   ) {
     this.token = this.cookieService.get('token');
+    this.headers = new HttpHeaders({ 'Authorization': `Bearer ${this.token}`, 'Content-Type': 'application/json' })
   }
 
   getCurriculum(id: number): Observable<Curriculum> {
-    return this.httpClient.get<Curriculum>(this.cvUrl + `/${id}`, {
-      headers: {
-        'Authorization': `Bearer ${this.token}`
-      }
-    });
+    return this.httpClient.get<Curriculum>(this.cvUrl + `/${id}`, { headers: this.headers });
   }
 
   createCurriculum(cv: Curriculum): Observable<Curriculum> {
-    return this.httpClient.post<Curriculum>(this.cvUrl, cv, {
-      headers: {
-        'Authorization': `Bearer ${this.token}`
-      }
-    });
+    return this.httpClient.post<Curriculum>(this.cvUrl, cv, { headers: this.headers });
   }
 
   updateCurriculum(cv: Curriculum, id: number): Observable<Curriculum> {
-    return this.httpClient.put<Curriculum>(this.cvUrl + `/${id}`, cv, {
-      headers: {
-        'Authorization': `Bearer ${this.token}`
-      }
-    });
+    return this.httpClient.put<Curriculum>(this.cvUrl + `/${id}`, cv, { headers: this.headers });
   }
 
   deleteCurriculum(id: number): Observable<any> {
-    return this.httpClient.delete<any>(this.cvUrl + `${id}`, {
-      headers: {
-        'Authorization': `Bearer ${this.token}`
-      }
-    });
+    return this.httpClient.delete<any>(this.cvUrl + `${id}`, { headers: this.headers });
   }
 
   downloadCurriculum(id: number, template: string): Observable<Blob> {
     return this.httpClient.get(this.cvUrl + `/${id}/pdf?template=${template}`, {
-      responseType: 'blob',
-      headers: {
-        'Authorization': `Bearer ${this.token}`
-      }
+      responseType: 'blob', headers: this.headers
     });
   }
 
