a6a3f5b8cb1b6549c1593869c777de2c4e54e6aa
carlosmz96 <carlosmz96esp@gmail.com>
2025-10-25 11:34:29 +0200

    Commit inicial

---
+++ cvgen-api/src/main/java/dev/carlosmz96/cvgen/cvgen_api/CvgenApiApplication.java
@@ -0,0 +1,13 @@
+package dev.carlosmz96.cvgen.cvgen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvgenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvgenApiApplication.class, args);
+	}
+
+}
---
+++ cvgen-api/src/test/java/dev/carlosmz96/cvgen/cvgen_api/CvgenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carlosmz96.cvgen.cvgen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvgenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
