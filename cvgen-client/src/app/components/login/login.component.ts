9c51df330909657fe6fa6d68e1b3fd1c6f1d5023
carlosmz96 <carlosmz96esp@gmail.com>
2025-10-26 22:21:27 +0100

    Construcción del formulario de login e integración con backend mediante la petición de login

--- cvgen-client/src/app/components/login/login.component.ts
+++ cvgen-client/src/app/components/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  login() {
+    this.authService.login(this.email, this.password).subscribe(
+      {
+        next: res => {
+          this.authService.saveSession(res);
+          this.router.navigate(['/']);
+        },
+        error: err => {
+          console.error('Error al iniciar sesión', err);
+        }
+      }
+    );
+  }
 
 }
