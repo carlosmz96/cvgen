d70a8d1771c25f57c0444cd5402e720cc9066bf8
carlosmz96 <carlosmz96esp@gmail.com>
2025-10-27 18:04:18 +0100

    Formulario de registro con mismos estilos que el login. Uso de password e inputs de PrimeNG.

--- cvgen-client/src/app/components/login/login.component.ts
+++ cvgen-client/src/app/components/login/login.component.ts
@@ -3,10 +3,21 @@ import { Router, RouterLink } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 
 import { AuthService } from '../../services/auth/auth.service';
+import { PasswordModule } from 'primeng/password';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule],
+  imports: [
+    RouterLink,
+    FormsModule,
+    PasswordModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -18,7 +29,7 @@ export class LoginComponent {
   constructor(
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) { }
 
   login() {
     this.authService.login(this.email, this.password).subscribe(
--- cvgen-client/src/app/components/register/register.component.ts
+++ cvgen-client/src/app/components/register/register.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../../services/auth/auth.service';
+
+import { PasswordModule } from 'primeng/password';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    RouterLink,
+    FormsModule,
+    PasswordModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  register() {
+    if (this.password !== this.confirmPassword) {
+      console.error('Las contraseñas no coinciden');
+      return;
+    }
+
+    const userData = { name: this.name, email: this.email, password: this.password };
+
+    this.authService.register(userData).subscribe({
+      next: res => {
+        console.log('Registro exitoso', res);
+        this.router.navigate(['/login']);
+      },
+      error: err => {
+        console.error('Error al registrar usuario', err);
+      }
+    });
+  }
 
 }
